74cb8664f9c0b07bb5fc0cba259604ae940e259e
Rafael Moraes <rafaelmoraes005@gmail.com>
2024-06-07 15:02:33 -0300

    [feat] novo componente PageHome - Página Home da aplicação

---
+++ src/pages/home/PageHome.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+      <RouterLink to="/">
+        <h1>Home</h1>
+      </RouterLink>
+      <RouterLink to="/login">
+        <h1>Login</h1>
+      </RouterLink>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'PageHome'
+}
+</script>
+
+<style lang="scss" scoped>
+
+h1{
+  color: var(--light);
+}
+</style>
+
